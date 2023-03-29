f82317fc01be7cb7d4da94be8c6ffaf9b0a869e0
Berlin <50ta2012@gmail.com>
2023-03-29 15:58:35 +0800

    登入頁面基本 layout

--- client/src/web/auth/Login.tsx
+++ client/src/web/auth/Login.tsx
@@ -1,5 +1,45 @@
+import { Box, TextField, Button, Grid } from "@mui/material";
+import "./Login.css";
 function Login() {
-	return <div>Login</div>;
+	return (
+		<>
+			<Box
+				style={{ marginTop: "10rem" }}
+				id="box"
+				component="form"
+				sx={{
+					m: "auto",
+					px: 3,
+					py: 5,
+					"& > :not(style)": { m: 2 },
+					width: "fit-content",
+					border: 1,
+					borderRadius: "15px",
+				}}
+				noValidate
+				autoComplete="off"
+			>
+				<div>
+					<div style={{ textAlign: "center" }}>達運光電人臉辨識系統</div>
+				</div>
+				<div>
+					<TextField placeholder="Account" size="small"></TextField>
+				</div>
+				<div>
+					<TextField
+						placeholder="Password"
+						type="password"
+						size="small"
+					></TextField>
+				</div>
+				<div>
+					<Button variant="contained" fullWidth>
+						Login
+					</Button>
+				</div>
+			</Box>
+		</>
+	);
 }
 
 export default Login;
